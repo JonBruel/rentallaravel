b35dd87120b399c59396938e046a1c2a3ee61243
jbr <jbr@consiglia.dk>
2019-08-06 09:18:14 +0200

    More details added in the list showing the registrated identitypapers.

--- resources/views/myaccount/editidentitypapers.blade.php
+++ resources/views/myaccount/editidentitypapers.blade.php
@@ -2,13 +2,14 @@
 @section('content')
     <h3>{{__('Trip passport details') . ', ' .  $contractdescription}}</h3>
     <p>{{ __('Identitypaper gdpr statement') }}</p>
+    <br />
     @if(sizeof($models) > 0)
         <!-- Table showing existing passport numbers -->
         <div class="table-responsive">
-            <table class="table table-striped">
+            <table class="table table-striped table-sm">
                 <tr>
                     <th>
-                        {{ __('Edit') }}
+                        {{ __('Edit') }}/{{ __('Delete') }}
                     </th>
                     <th>
                         {{ ucfirst(__('Forename') )}}
@@ -18,6 +19,18 @@
                     </th>
                     <th>
                         {{ ucfirst(__('Passportnumber')) }}
+                    </th>
+                    <th>
+                        {{ ucfirst(__('Sex')) }}
+                    </th>
+                    <th>
+                        {{ ucfirst(__('Dateofissue')) }}
+                    </th>
+                    <th>
+                        {{ ucfirst(__('Dateofbirth')) }}
+                    </th>
+                    <th>
+                        {{ ucfirst(__('Country')) }}
                     </th>
                 </tr>
                 @foreach($models as $model)
@@ -34,21 +47,35 @@
                         <td>
                             {{ $model->passportnumber}}
                         </td>
+                        <td>
+                            {{ $model->sex}}
+                        </td>
+                        <td>
+                            {{ $model->dateofissue->format('Y-m-d')}}
+                        </td>
+                        <td>
+                            {{ $model->dateofbirth->format('Y-m-d')}}
+                        </td>
+                        <td>
+                            {{ $model->country}}
+                        </td>
                     </tr>
                 @endforeach
                 </tbody>
             </table>
         @endif
-        <!-- Form for new passposr numbers -->
+        <!-- Form for new passport numbers -->
+        <br />
+        <div  class="container rounded" style="border:1px solid black">
         <h3>{{__('Add guest passport details')}}</h3>
         <div class="table-responsive table-sm">
             {!! Form::model($newidentitypaper, ['action' => ['MyAccountController@saveidentitypaper', $newidentitypaper]]) !!}
             <br />
-            <br />
-
             @include('partials.two_column_edit_1', ['model' => $newidentitypaper, 'errors' => $errors, 'fields' => $fields, 'vattr' => $vattr])
             {!! Form::submit(__('Add new identity paper'),['class' => "btn btn-primary"]); !!}
+            <br /><br />
             {!! Form::close() !!}
+        </div>
         </div>
         @include('partials.client_validation')
 
